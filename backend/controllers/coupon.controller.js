cd90a92055e4f8fbd389ccb3bcc9020dbb7795e3
gaurabpokhrel09 <gaurabpokhrl@gmail.com>
2025-02-16 00:16:00 +0545

    Completed Backend

---
+++ backend/controllers/coupon.controller.js
@@ -0,0 +1,42 @@
+import Coupon from "../models/coupon.model.js";
+
+export const getCoupon = async (req, res) => {
+  try {
+    const coupon = await Coupon.findOne({
+      userId: req.user._id,
+      isActive: true,
+    });
+    res.json(coupon || null);
+  } catch (error) {
+    console.log("Error in getting coupon", error.message);
+    res.status(500).json({ message: "Server Error", error: error.message });
+  }
+};
+
+export const validateCoupon = async (req, res) => {
+  try {
+    const { code } = req.body;
+    const coupon = await Coupon.findOne({
+      code: code,
+      userId: req.user._id,
+      isActive: true,
+    });
+    if (!coupon) {
+      return res.status(400).json({ message: "Invalid Coupon" });
+    }
+    if (coupon.expiryDate < new Date()) {
+      coupon.isActive = false;
+      await coupon.save();
+      return res.status(400).json({ message: "Coupon Expired" });
+    }
+
+    res.json({
+      message: "Coupon is Valid",
+      code: coupon.code,
+      discountPercentage: coupon.discountPercentage,
+    });
+  } catch (error) {
+    console.log("Error in validating coupon", error.message);
+    res.status(500).json({ message: "Server Error", error: error.message });
+  }
+};
